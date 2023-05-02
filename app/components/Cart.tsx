fbcbe2cd0907096edebd962cc6a56c3a39c07361
Davey Schulz <daveschulz@hotmail.nl>
2023-05-02 20:12:12 +0200

    Added the functionality to remove and update products in the cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -4,6 +4,7 @@ import { FC } from 'react';
 import { useCartStore } from '@/store';
 import Image from 'next/image';
 import formatPrice from '@/util/price-format';
+import { IoAddCircle, IoRemoveCircle } from 'react-icons/io5';
 
 const Cart: FC = ({}) => {
   const cartStore = useCartStore();
@@ -29,7 +30,35 @@ const Cart: FC = ({}) => {
             />
             <div>
               <h2>{item.name}</h2>
-              <h2>Quantity: {item.quantity}</h2>
+              <div className="flex gap-1">
+                <h2>Quantity: {item.quantity}</h2>
+                <button
+                  onClick={() =>
+                    cartStore.removeProduct({
+                      id: item.id,
+                      name: item.name,
+                      image: item.image,
+                      unit_amount: item.unit_amount,
+                      quantity: item.quantity,
+                    })
+                  }
+                >
+                  <IoRemoveCircle />
+                </button>
+                <button
+                  onClick={() =>
+                    cartStore.addProduct({
+                      id: item.id,
+                      name: item.name,
+                      image: item.image,
+                      unit_amount: item.unit_amount,
+                      quantity: item.quantity,
+                    })
+                  }
+                >
+                  <IoAddCircle />
+                </button>
+              </div>
               <p className="text-sm">{formatPrice(item.unit_amount)}</p>
             </div>
           </div>
--- store.ts
+++ store.ts
@@ -7,6 +7,7 @@ interface CartState {
   cart: AddCartProps[];
   toggleCart: () => void;
   addProduct: (item: AddCartProps) => void;
+  removeProduct: (item: AddCartProps) => void;
 }
 
 export const useCartStore = create<CartState>()(
@@ -31,6 +32,29 @@ export const useCartStore = create<CartState>()(
             return { cart: updatedCart };
           } else {
             return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          }
+        }),
+      removeProduct: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find(
+            (cartItem) => cartItem.id === item.id,
+          );
+
+          if (existingItem && existingItem.quantity > 1) {
+            const updatedCart = state.cart.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return { ...cartItem, quantity: cartItem.quantity - 1 };
+              }
+              return cartItem;
+            });
+
+            return { cart: updatedCart };
+          } else {
+            const filteredCart = state.cart.filter(
+              (cartItem) => cartItem.id !== item.id,
+            );
+
+            return { cart: filteredCart };
           }
         }),
     }),
